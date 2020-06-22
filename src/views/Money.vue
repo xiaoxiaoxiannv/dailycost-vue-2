15a46b09787040b6870d9210ff415abd9c1d02e5
Luna <1017929760@qq.com>
2020-06-22 20:33:12 +0800

    写money.vue的tags部分的样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -50,3 +50,32 @@
     name: 'Money',
   };
 </script>
+
+<style lang="scss" scoped>
+    @import "~@/assets/style/helper.scss";
+    .tags{
+        font-size: 14px;
+        padding: 16px;
+        >.current{
+            display: flex;
+            >li{
+                background: #d9d9d9;
+                $h: 24px;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 16px;
+                margin-right: 12px;
+            }
+        }
+        >.new{
+            padding-top: 16px;
+            button{
+                background: transparent;
+                border: none;
+                color: #999;
+                border-bottom: 1px solid;
+                padding: 0 4px;
+            }
+        }
+    }
+</style>
